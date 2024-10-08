83fda9642e7d81b5e1f9e542a4201068cbaeaa26
FrancoFerrandi <139079062+FrancoFerrandi@users.noreply.github.com>
2024-10-07 21:56:50 -0300

    Update Punto_C.sh

--- Punto_C.sh
+++ Punto_C.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-cd /
-sudo mkdir Estructura_Asimetrica
-cd Estructura_Asimetrica/
-sudo mkdir -p {correo/{cartas{1..100},carteros{1..10}},clientes/cartas{1..100}}
-cd ..
-tree Estructura_Asimetrica
+
